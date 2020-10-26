6f3a5587f8483f02ec714cb6106fa6a1144f9163
Koi <ruozhizhanglife@gmail.com>
2020-10-25 18:40:04 -0700

    Added a new problem

---
+++ problems/graph/Path_With_Maximum_Minimum_Value.py
@@ -0,0 +1,49 @@
+'''
+https://leetcode.com/problems/path-with-maximum-minimum-value/
+
+Given a matrix of integers A with R rows and C columns, find the maximum score of a path starting at[0, 0] 
+and ending at[R-1, C-1].
+
+The score of a path is the minimum value in that path.  For example, the value of the path 8 →  4 →  5 →  9 
+is 4.
+
+A path moves some number of times from one visited cell to any neighbouring unvisited cell in one of the 4 
+cardinal directions(north, east, west, south).
+
+Example 1:
+Input: [[5, 4, 5], [1, 2, 6], [7, 4, 6]]
+Output: 4
+Explanation:
+The path with the maximum score is highlighted in yellow.
+
+Example 2:
+Input: [[2, 2, 1, 2, 2, 2], [1, 2, 2, 2, 1, 2]]
+Output: 2
+
+Example 3:
+Input: [[3, 4, 6, 3, 4], [0, 2, 1, 1, 7], [8, 8, 3, 2, 7],
+        [3, 2, 4, 9, 8], [4, 1, 2, 0, 0], [4, 6, 5, 4, 3]]
+Output: 3
+
+Note:
+1 <= R, C <= 100
+0 <= A[i][j] <= 10 ^ 9
+'''
+
+import heapq
+class Solution:
+    def maximumMinimumPath(self, A: List[List[int]]) -> int:
+        m, n = len(A), len(A[0])
+        dirs = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+        heap = [(-A[0][0], 0, 0)]
+        visited = [[False] * n for _ in range(m)]
+        visited[0][0] = True
+        while heap:
+            curScore, x, y = heapq.heappop(heap)
+            if x == m - 1 and y == n - 1:
+                return -curScore
+            for dx, dy in dirs:
+                nx, ny = x + dx, y + dy
+                if 0 <= nx < m and 0 <= ny < n and not visited[nx][ny]:
+                    visited[nx][ny] = True
+                    heapq.heappush(heap, (max(curScore, -A[nx][ny]), nx, ny))
